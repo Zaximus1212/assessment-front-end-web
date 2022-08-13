845e6906708dff667e3e0e06eddeefb2832e89cd
Zachary Boughton <zboughton@yahoo.com>
2022-08-13 01:21:41 -0600

    second commit, test finished

---
+++ profile.js
@@ -0,0 +1,8 @@
+const color = document.querySelector('#color')
+const place = document.querySelector('#place')
+const ritual = document.querySelector('#ritual')
+
+color.addEventListener('click', () => alert('red'))
+place.addEventListener('click', () => alert('Sagebrush Park'))
+ritual.addEventListener('click', () => alert('watching LOTR with my friends'))
+
